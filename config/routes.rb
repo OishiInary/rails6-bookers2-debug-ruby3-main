a733f0be7f10bbeab81bbbef2ec0238d5e539455
OishiInary <168375833+OishiInary@users.noreply.github.com>
2024-07-12 07:47:30 +0000

    [finish]

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
-  
-
   root to: "homes#top"
-  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
+  get "home/about"=>"homes#about", as: "about"
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+   resource :favorite, only: [:create, :destroy]
+   resources :book_comments, only: [:create, :destroy]
+  end
   
   resources :users, only: [:index,:show,:edit,:update]
-  get "home/about"=>"homes#about"
+
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
